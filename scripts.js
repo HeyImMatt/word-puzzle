76bb07acbb5cccebc1a9ae19900da95a7b1aa2de
heyimmatt <mrmcfarland@gmail.com>
2020-06-09 13:34:48 -0700

    Replace vowels in the input sentence with dashes and return the new string
    Co-authored-by: Ruthie Newman <ruthienewman@protonmail.com>

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
+//UI Logic
 $(document).ready(function () {
   $('form#user-sentence').submit(function (event) {
     event.preventDefault();
     const userSentence = $('#sentenceInput').val();
-    console.log(userSentence);
     const result = puzzle(userSentence);
     console.log(result)
   });
 });
 
+//Biz Logic
 function puzzle(sentence) {
   const puzzleSentence = sentence.split('');
-  console.log('Starting loop')
   for (let index = 0; index <= sentence.length - 1; index++) {
-    let letter = puzzleSentence[index];
+    let letter = puzzleSentence[index].toLowerCase();
     if (
       letter === 'a' ||
       letter === 'e' ||
@@ -21,18 +21,7 @@ function puzzle(sentence) {
       letter === 'u'
     ) {
       puzzleSentence.splice(index, 1, '-')
-      console.log(puzzleSentence)
-    } else if (letter = ' ') {
-      puzzleSentence.splice(index, 1, ' ')
-      console.log(puzzleSentence)
-    } else {
-      puzzleSentence.splice(index, 1, letter)
-      console.log(puzzleSentence)
-    }
+    } 
   }
-  console.log('End of the loop')
-  return puzzleSentence.join(' ');
+  return puzzleSentence.join('');
 }
-
-//"this is a sentence"
-//"th-s -s - s-nt-nc-"
